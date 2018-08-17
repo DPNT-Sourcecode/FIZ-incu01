35050fe7c0cd1f9e541fdb4d88ac1cb668fef79d
sbx_user1060 <sbx_user1060@ip-10-129-62-35.eu-west-2.compute.internal>
2018-08-17 12:15:27 +0000

    Fri Aug 17 12:15:27 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,17 +1,18 @@
 package befaster.solutions.FIZ;
 
-
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
         boolean containsThree = number.toString().contains("3");
         boolean containsFive = number.toString().contains("5");
-        boolean containsBoth = containsThree&&containsFive;
-        System.out.println(containsBoth);
+        boolean containsBoth = containsThree && containsFive;
         if (number % 15 == 0 || (containsBoth)) {
             return "fizz buzz";
-        } else
-        if (number % 3 == 0 || containsThree) {
+        } else if (number % 5 == 0 && containsThree) {
+            return "fizz buzz";
+        } else if (number % 3 == 0 && containsFive) {
+            return "fizz buzz";
+        } else if (number % 3 == 0 || containsThree) {
             return "fizz";
         } else if (number % 5 == 0 || containsFive) {
             return "buzz";
