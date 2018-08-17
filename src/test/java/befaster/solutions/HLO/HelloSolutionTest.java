171f5c894b11f5c364d1a841f23b89374a92e996
sbx_user1060 <sbx_user1060@ip-10-129-62-35.eu-west-2.compute.internal>
2018-08-17 11:40:36 +0000

    Fri Aug 17 11:40:36 UTC 2018

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -5,39 +5,33 @@
  */
 package befaster.solutions.HLO;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  *
  * @author niyioladeji
  */
 public class HelloSolutionTest {
-    
-    public HelloSolutionTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+
+    private HelloSolution helloSolution;
+
     @Before
     public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+        helloSolution = new HelloSolution();
     }
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void hello() {
+        String friendsName = new String();
+        assertThat(helloSolution.hello(friendsName), equalTo("Hello World!"));
+    }
 }
